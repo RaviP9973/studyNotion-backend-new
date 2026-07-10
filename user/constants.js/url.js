fa8fedb6c66b4824f2f2d3336e27e2290b9fe869
RviP9973 <rp031776@gmail.com>
2026-07-10 11:48:52 +0530

    fixed frontend url

--- user/constants.js/url.js
+++ user/constants.js/url.js
@@ -2,4 +2,4 @@ import dotenv from "dotenv"
 dotenv.config();
 
 const nodeEnv = process.env.NODE_ENV || "development"
-export const frontendUrl = nodeEnv === "development" ? "http://localhost:5173" : "http://study-notion.duckdns.org";
+export const frontendUrl = nodeEnv === "development" ? "http://localhost:5173" : "https://study-notion-frontend-two-olive.vercel.app";
