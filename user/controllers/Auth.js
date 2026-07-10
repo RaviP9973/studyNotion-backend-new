7236d8cac83aed008be24695a669dfb3f76020e6
RviP9973 <rp031776@gmail.com>
2026-07-10 11:51:41 +0530

    fixed auth error

--- user/controllers/Auth.js
+++ user/controllers/Auth.js
@@ -232,6 +232,8 @@ export const login = async (req, res) => {
       const options = {
         expires: new Date(Date.now() + 24 * 60 * 60 * 1000),
         httpOnly: true,
+        secure: true,
+        sameSite: "none"
       };
       res.cookie("token", token, options).status(200).json({
         success: true,
